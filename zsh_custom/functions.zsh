e1e1236f969d51abffe5c67157ef76fdc86e6c7a
Michal Karas <michal.karas@allegro.com>
2026-04-13 11:36:17 +0200

    Add git-relocate function

--- zsh_custom/functions.zsh
+++ zsh_custom/functions.zsh
@@ -19,5 +19,20 @@ function nvm_prompt_info() {
   echo ""
 }
 
+function git-relocate() {
+    if [ "$#" -ne 3 ]; then
+        echo "Error: Missing arguments."
+        echo "Usage:  git-relocate <branch-to-move> <old-parent> <new-parent>"
+        echo "Example: git-relocate feature-x master develop"
+        return 1
+    fi
 
+    local branch=$1
+    local old_parent=$2
+    local new_parent=$3
+
+    echo "Relocating $branch from $old_parent onto $new_parent..."
+    
+    git rebase --onto "$new_parent" "$old_parent" "$branch"
+}
 
